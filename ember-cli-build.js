46c070053f06731834c50a41034bd21905182367
carlogilmar <carlogilmar12@gmail.com>
2017-11-08 09:58:30 -0600

    #1 Adding boostrap in bower and ember-cli-build

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -6,6 +6,7 @@ module.exports = function(defaults) {
     // Add options here
   });
 
+
   // Use `app.import` to add additional libraries to the generated
   // output files.
   //
@@ -18,6 +19,8 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
+
+  app.import('bower_components/bootstrap/dist/css/bootstrap.css');
 
   return app.toTree();
 };
